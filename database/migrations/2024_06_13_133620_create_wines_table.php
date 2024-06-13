2604ed0bafa388a52c979102c9af1882c258788e
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-13 15:46:19 +0200

    feat: table migration

--- database/migrations/2024_06_13_133620_create_wines_table.php
+++ database/migrations/2024_06_13_133620_create_wines_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('wines', function (Blueprint $table) {
             $table->id();
+            $table->string('winery');
+            $table->string('wine');
+            $table->string('location');
+            $table->string('image');
             $table->timestamps();
         });
     }
