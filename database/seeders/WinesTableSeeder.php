12a7ca0f9b1645932194f415c818b82d56828a7b
Emilio Gallo <emilio.gallo17@gmail.com>
2024-06-13 17:05:35 +0200

    feat: added raw structure of seeder - WinesTableSeeder.php

---
+++ database/seeders/WinesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Wine;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+
+class WinesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $response = Http::get('https://api.sampleapis.com/wines/reds');
+
+        // $data = $response->json();
+
+        // foreach ($data as  $wine) {
+
+        //     $newWine = new Wine();
+
+        //     $newWine->winery = $wine->winery;
+        //     $newWine->wine = $wine->wine;
+        //     $newWine->location = $wine->location;
+        //     $newWine->image = $wine->image;
+
+        //     $newWine->save();
+        // };
+
+    }
+}
