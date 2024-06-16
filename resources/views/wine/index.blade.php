ad99513e533ad73597d0a3cbf648719719650bb2
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-17 00:32:47 +0200

    fix: fixed all cards

--- resources/views/wine/index.blade.php
+++ resources/views/wine/index.blade.php
@@ -1,34 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-   <div class="container-md">
-      <h1>Wine List</h1>
+    <section>
+        <div class="container text-center">
+            <h1 class="py-5">Wines List</h1>
+            <div class="row g-3">
+               @foreach ($winesObj as $wine)
+                    <div class="col-12 col-md-6 col-lg-4 mb-4">
+                        <div class="card">
+                           <picture>
+                              <img src="{{ $wine->image }}" class="img-fluid" alt="{{ $wine->image }}">
+                            </picture>
+                            <div class="card-body">
 
-      <div class="row">
+                                <h5 class="card-title">Wine: {{ $wine->wine }}</h5>
 
-         @foreach ($winesObj as $wine)
-            <div class="col-4">
-
-               <div class="card">
-
-                  <img src="{{ $wine->image }}" class="img-fluid" alt="{{ $wine->wine }}">
-
-                  <div class="card-body">
-
-                     <h5 class="card-title">Wine: {{ $wine->wine }}</h5>
-
-                     <h4 class="card-subtitle">Winery: {{ $wine->winery }}</h4>
-
-                     <h4 class="card-subtitle">Location: {{ $wine->location }}</h4>
-
-                  </div>
-
-               </div>
-
+                                <h4 class="card-subtitle">Winery: {{ $wine->winery }}</h4>
+    
+                                <h4 class="card-subtitle">Location: {{ $wine->location }}</h4>
+    
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-         @endforeach
-
-      </div>
-
-   </div>
+        </div>
+    </section>
 @endsection
