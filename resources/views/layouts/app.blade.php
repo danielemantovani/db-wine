63fee59d4f3d54e640400d1748410ad0087295e3
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-17 00:12:47 +0200

    feat: added favicon and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Wines Selection</title>
+    <link rel="icon" type="image/x-icon" href="{{ Vite::asset('resources/img/wines.jpg')}}">
 
     @vite('resources/js/app.js')
 </head>
