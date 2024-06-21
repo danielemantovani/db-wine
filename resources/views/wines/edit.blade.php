01fe5da2c1e0979abbe528a2823891769106a615
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-21 16:49:14 +0200

    feat: added editing form and button for editing

--- resources/views/wines/edit.blade.php
+++ resources/views/wines/edit.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center">Edit Page</h1>
-    {{-- <div class="container">
-        <form action=" {{ route('wines.store') }} " method="POST">
+    <h1 class="text-center">Modify Page</h1>
+
+     <div class="container">
+        <form action=" {{ route('wines.update', ['wine' => $wine->id]) }} " method="POST">
             @csrf
+            @method('PUT')
             
             <div class="mb-3">
                 <label for="winery" class="form-label">Winery</label>
-                <input type="text" class="form-control" id="winery" name="winery">
+                <input type="text" class="form-control" id="winery" name="winery" value="{{ $wine->winery }}">
             </div>
             <div class="mb-3">
                 <label for="wine" class="form-label">Wine</label>
-                <input type="text" class="form-control" id="wine" name="wine">
+                <input type="text" class="form-control" id="wine" name="wine" value="{{ $wine->wine }}">
             </div>
             <div class="mb-3">
                 <label for="location" class="form-label">Location</label>
-                <input type="text" class="form-control" id="location" name="location">
+                <input type="text" class="form-control" id="location" name="location" value="{{ $wine->location }}">
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Image</label>
-                <input type="text" class="form-control" id="image" name="image">
+                <input type="text" class="form-control" id="image" name="image" value="{{ $wine->image }}">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
