9f12b825b9f13f7c7e6ac42b14ccac9d75c9d5cb
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-21 17:35:55 +0200

    feat: added navbar - WIP

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Wines DB</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="{{ route('wines.index') }}">Winery</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
