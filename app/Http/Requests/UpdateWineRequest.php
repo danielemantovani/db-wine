7fd16bbc75233c69981928d473c7178454be6c87
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-27 17:00:53 +0200

    feat:  create update request

---
+++ app/Http/Requests/UpdateWineRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateWineRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'winery' => ['required', 'min:5'],
+            'wine' => ['required', 'min:5'],
+            'location' => ['nullable'],
+            'image' => ['nullable'],
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'winery.required' => 'Inserire la cantina',
+            'winery.min' => 'Inserire almeno 5 caratteri per la cantina',
+            'wine.required' => 'Inserire il nome del vino',
+            'wine.min' => 'Inserire almeno 5 caratteri per il nome del vino',
+        ];
+    }
+}
