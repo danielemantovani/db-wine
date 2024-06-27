4415c550b6f5eeda8af9fa1aad001374d7ef2217
LucaElisio <personal.elix@gmail.com>
2024-06-27 16:18:13 +0200

    feat: background style and fixed header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center">Home Page</h1>
+    <div class="container mt-5">
+        <h1 class="text-center pt-5">Home Page</h1>
+    </div>
 @endsection
